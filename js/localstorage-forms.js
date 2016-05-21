8fc5562d65e6cb1e0c83484898b211bc1516ddf4
Courtney Loveday <courtneyloveday94@gmail.com>
2016-05-22 10:51:43 +1200

    finished the basic phone contact page and other minor fixes

--- js/localstorage-forms.js
+++ js/localstorage-forms.js
@@ -1,28 +1,5 @@
 // name, email, contact-message, chat-username, profile-pic-url
 
-// CHAT FORM
-
-// does this user have a chat-username saved?
-if(localStorage.getItem('chat-username')){
-	// put the name in the chat-username field
-	document.querySelector('#chat-username').value=localStorage.getItem('chat-username');
-}
-// listen for input on the chat-username field
-document.querySelector('#chat-username').onkeyup=function(){
-	localStorage.setItem('chat-username', this.value);
-}
-
-// does this user have a profile picture url saved?
-if(localStorage.getItem('profile-pic-url')){
-	// put the url in the profile-pic-url field
-	document.querySelector('#profile-pic-url').value=localStorage.getItem('profile-pic-url');
-}
-// listen for input on the profile-pic-url field
-document.querySelector('#profile-pic-url').onkeyup=function(){
-	localStorage.setItem('profile-pic-url', this.value);
-}
-
-
 // CONTACT FORM
 
 // does this user have a name saved?
